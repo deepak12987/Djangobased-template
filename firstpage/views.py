138b68372edf92662057b7890cd305a2aee94226
deepak12987 <67735663+deepak12987@users.noreply.github.com>
2020-08-07 08:11:12 +0530

    Add files via upload
    First demo website using Django

---
+++ firstpage/views.py
@@ -0,0 +1,61 @@
+#"coded my me"
+from django.http import HttpResponse
+from django.shortcuts import render
+def index(request):
+    return render(request,'one.html')
+def analysis(request):
+    djtext = request.POST.get("text","default")
+    resp = request.POST.get("removepunc","off")
+    uppe = request.POST.get("uppercase","off")
+    newline = request.POST.get("removenewline","off")
+    extraspace = request.POST.get("extraspaceremove","off")
+    if resp == "on":
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed + char
+        djtext = analyzed
+        params = {'analyzed_text':analyzed}
+
+    if uppe == "on":
+        analyzed = ""
+        for char in djtext:
+            analyzed += char.upper()
+        djtext = analyzed
+        params = {'analyzed_text':analyzed}
+    
+    if newline == "on":
+        analyzed = ""
+        for char in djtext:
+            if char != '\n' and char != '\r':
+                analyzed += char
+        djtext = analyzed
+        params = {'analyzed_text':analyzed}
+
+    if extraspace == "on":
+        analyzed = ""
+        for i,char in enumerate(djtext):
+            if djtext[i] == " " and djtext[i+1] == " ":
+                continue
+            else:
+                analyzed += char
+        djtext = analyzed
+        params = {'analyzed_text':analyzed}
+    
+
+    if resp != "on" and extraspace != "on"  and newline != "on" and uppe != "on":
+        return HttpResponse("error")
+    return render(request,'analyze.html',params)
+
+      
+
+
+def about(request):
+    return render(request,'about.html')
+
+def contact(request):
+    return render(request,'contact.html')  
+    
+
+
